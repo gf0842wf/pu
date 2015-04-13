519590e7e5836fafd0a3eef0b67c158d64c8ba0e
wangfei <gf0842wf@gmail.com>
2015-04-13 09:52:20 +0800

    group

--- pu/util.py
+++ pu/util.py
@@ -70,6 +70,18 @@ def deep_decode(ob, encoding='utf_8', errors='strict'):
         return ob
 
 
+def group(seq, chunk_size):
+    """Split a sequence into chunks.
+
+    Source: http://stackoverflow.com/a/312464/1158494
+
+    :param seq: sequence to be split.
+    :param chunk_size: chunk size.
+    """
+    for i in range(0, len(seq), chunk_size):
+        yield seq[i:i + chunk_size]
+
+
 def xfind_all(s, sep, start=None, end=None):
     """ 返回所有index
     >>> list(xfind_all('abcdabcdabcd', 'bc'))
@@ -85,7 +97,7 @@ def xfind_all(s, sep, start=None, end=None):
             break
         _start = index + len(sep)
 
-    
+
 def xsplit(s, sep, maxsplit=None):
     """ split的generator版本
     >>> list(xsplit('abcdabcdabcd', 'bc'))
@@ -109,8 +121,8 @@ def xsplit(s, sep, maxsplit=None):
             break
     if pos != -1:
         yield s[pos:]
-        
-        
+
+
 if __name__ == '__main__':
     import doctest
 
